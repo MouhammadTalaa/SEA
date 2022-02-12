b73a123ff4249a3651811c2d5dc8edbc3cdc327b
MouhammadTalaa <mouhammad.talaa@gmail.com>
2022-02-12 16:01:48 +0200

    migrations

--- database/migrations/2022_02_12_130824_create_services_table.php
+++ database/migrations/2022_02_12_130824_create_services_table.php
@@ -15,6 +15,12 @@ class CreateServicesTable extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('link');
+            $table->string('author');
+            $table->string('image');
+            $table->string('specialize');
+            $table->enum('type', ['book', 'service','course']);
             $table->timestamps();
         });
     }
